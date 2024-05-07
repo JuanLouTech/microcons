93aa17077149886fdfba7443cce68b8e51ea6a69
Juan_Lou <mettheland@gmail.com>
2024-05-07 21:44:30 +0200

    Sprites and 8 buttons

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,6 @@ int readIntFromEEPROM(int address)
 /*
 --------------------------------
 */
-
 String text;
 int width = 128;
 int height = 64;
@@ -26,20 +25,48 @@ int height = 64;
 Adafruit_SSD1306 display(width, height, &Wire, -1);
 
 bool screenReady = true;
-int errorLed = 15;
-int leftButton = 5;
-int rightButton = 4;
-int dPad = 20;
 
-struct InputMapping {
-  float up;
-  float down;
-  float left;
-  float right;
-  float enter;
+// PIN ASSIGNMENTS
+
+// TODO: Review this assignments
+int leftButton = 8;
+int rightButton = 10;
+int upButton = 9;
+int downButton = 11;
+int wButton = 5;
+int nButton = 6;
+int eButton = 7;
+int sButton = 4;
+
+
+// SPRITES
+
+// Ghost (8 x 9)
+const unsigned char  ghostRight [] PROGMEM = {
+	0x3c, 0x7e, 0xff, 0xed, 0xed, 0xff, 0xff, 0xff, 0xd5
 };
 
-InputMapping controls = {3.0, 50.0, 2.0, 6.0, 1.0};
+const unsigned char ghostLeft [] PROGMEM = {
+	0x3c, 0x7e, 0xff, 0xb7, 0xb7, 0xff, 0xff, 0xff, 0xab
+};
+
+// Pacman(9 x 9)
+const unsigned char pacmanOpenLeft [] PROGMEM = {
+	0x3e, 0x00, 0x7f, 0x00, 0x77, 0x80, 0x1f, 0x80, 0x07, 0x80, 0x0f, 0x80, 0x1f, 0x80, 0x3f, 0x00, 
+	0x3e, 0x00
+};
+
+const unsigned char pacmanOpenRight [] PROGMEM = {
+	0x3e, 0x00, 0x7f, 0x00, 0xf7, 0x00, 0xfc, 0x00, 0xf0, 0x00, 0xf8, 0x00, 0xfc, 0x00, 0x7e, 0x00, 
+	0x3e, 0x00
+};
+
+const unsigned char pacmanShut [] PROGMEM = {
+	0x3e, 0x00, 0x7f, 0x00, 0xf7, 0x80, 0xff, 0x80, 0xff, 0x80, 0xff, 0x80, 0xff, 0x80, 0x7f, 0x00, 
+	0x3e, 0x00
+};
+
+// GAME VARIABLES
 
 int verticalLimits[] = {0, 64};
 int horizontalLimits[] = {0, 128};
@@ -48,7 +75,7 @@ int playerPosition[] = {60,64 - playerSize[1]};
 int moveSpeed = 2;
 int verticalVelocity = 0;
 int gravity = 1;
-int jumpForce = 8;
+int jumpForce = 7;
 int playerDirection = 1;
 bool canJump = true;
 
@@ -67,7 +94,7 @@ int enemyDirection = 1;
 int enemyBounces = 3;
 bool enemyMouthOpen = false;
 int enemyMouthSize[] = {2, 4};
-unsigned long enemyMouthOpenTime = 500;
+unsigned long enemyMouthOpenTime = 100;
 bool isEnemyActive = false;
 unsigned long lastEnemyActiveTime = 0;
 unsigned long timeForNextEnemyActive = 5000;
@@ -95,18 +122,18 @@ void resetGame();
 
 void setup() {
   Serial.begin(115200);
-  pinMode(leftButton, INPUT_PULLUP);
-  pinMode(rightButton, INPUT_PULLUP);
-  pinMode(errorLed, OUTPUT);
-  pinMode(dPad, INPUT);
+  pinMode(leftButton, INPUT);
+  pinMode(rightButton, INPUT);
+  pinMode(upButton, INPUT);
+  pinMode(downButton, INPUT);
 
 
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
     screenReady = false;
-    digitalWrite(errorLed, HIGH);
     return;
   }
+
   resetGame();
 
   display.clearDisplay();
@@ -123,7 +150,7 @@ void resetGame() {
   lastEnemyActiveTime = millis();
   timeForNextEnemyActive = random(4000, 10000);
   isGameOver = false;
-  highScore = readIntFromEEPROM(highScoreAddress);
+  highScore = 16;
 }
 
 /*
@@ -187,11 +214,6 @@ void createEnemy() {
 /* 
 INPUT READING AND PLAYER MOVEMENT 
 */
-int input() {
-  int dPadInput = analogRead(dPad);
-  // This cleans and stabilize the input from the dPad.
-  return ceil(map(dPadInput, 0, 1023, 0, 100)/2.0);
-}
 
 void getInput()
 {
@@ -201,17 +223,15 @@ void getInput()
     canJump = true;
   }
 
-  if (input() == float(controls.left) || input() == float(controls.left + controls.up)) {
+  if (digitalRead(leftButton) == HIGH) {
     movementX = -moveSpeed;
     playerDirection = -1;
   }
-  if (input() == float(controls.right)|| input() == float(controls.right + controls.up)) {
+  if (digitalRead(rightButton) == HIGH) {
     movementX =  moveSpeed;
     playerDirection = 1;
   }
-  if ((input() == float(controls.up) ||
-   input() == float(controls.right + controls.up) ||
-   input() == float(controls.left + controls.up)) && canJump) {
+  if (digitalRead(sButton) == HIGH && canJump) {
     verticalVelocity = -jumpForce;
     canJump = false;
   }
@@ -303,24 +323,8 @@ void moveEnemy() {
 PAINT STUFF FUNCTIONS
 */
 void drawGhost(int  position[2], int size[2], int direction) {
-   // Body (x: 0, y: 2, size: 8x6)
-  display.fillRect(position[0], position[1] + 2, size[0], size[1] - 2, SSD1306_WHITE);
-  // Head 1 (x: 1, y: 1, size: 6x1)
-  display.fillRect(position[0] + 1, position[1] + 1, size[0] - 2, 1, SSD1306_WHITE);
-  // Head 2 (x: 0, y: 2, size: 4x1)
-  display.fillRect(position[0] + 2, position[1], size[0] - 4, 1, SSD1306_WHITE);
-
-  int faceXOffset = direction == 1 ? 3 : 0;
-  // Left eye (x: offset, y: 3, size: 2x2)
-  display.fillRect(position[0] + faceXOffset, position[1] + 3, 2, 2, SSD1306_BLACK);
-  // Right eye (x: offset + 2, y: 3, size: 2x2)
-  display.fillRect(position[0] + faceXOffset + 3, position[1] + 3, 2, 2, SSD1306_BLACK);
-  // Base cut 1 (x: offset - 1, y: size[0] - 1, size: 1x1)
-  display.fillRect(position[0] + faceXOffset - 1, position[1] + size[1] - 1, 1, 1, SSD1306_BLACK);
-  // Base cut 2 (x: offset + 2, y: size[0] - 1, size: 1x1)
-  display.fillRect(position[0] + faceXOffset + 2, position[1] + size[1] - 1, 1, 1, SSD1306_BLACK);
-  // Base cut 3 (x: offset + 4, y: size[0] - 1, size: 1x1)
-  display.fillRect(position[0] + faceXOffset + 4, position[1] + size[1] - 1, 1, 1, SSD1306_BLACK);
+  if (direction == 1)  display.drawBitmap(position[0], position[1], ghostRight, 8, 9, SSD1306_WHITE);
+  else display.drawBitmap(position[0], position[1], ghostLeft, 8, 9, SSD1306_WHITE);
 }
 
 
@@ -343,17 +347,13 @@ void drawFruit()
 
 void drawEnemy() {
   if (!isEnemyActive) return;
-  // Enemy looks like packman, with one visible eye and the mouth open and closes
-  // Body
-  display.fillCircle(enemyPosition[0] + enemyRadius, enemyPosition[1] + enemyRadius, enemyRadius, SSD1306_WHITE);
-  // Eye (depending on the direction of moving)
-  int eyeOffset = enemyDirection == 1 ? 1 : -3;
-  display.fillRect(enemyPosition[0] + enemyRadius + eyeOffset, enemyPosition[1] + enemyRadius - 2, 2, 2, SSD1306_BLACK);
-
-  if (!enemyMouthOpen) return;
-  int mouthOffset = enemyDirection == 1 ? enemyRadius + 3 : 0;
-  display.fillRect(enemyPosition[0] + mouthOffset, enemyPosition[1] + enemyRadius - 2, 2, 4, SSD1306_BLACK);
-  display.drawLine(enemyPosition[0] + mouthOffset + 2, enemyPosition[1] + enemyRadius, enemyPosition[0] + mouthOffset + 2, enemyPosition[1] + enemyRadius + 4, SSD1306_BLACK);
+  
+  if (!enemyMouthOpen) 
+    display.drawBitmap(enemyPosition[0], enemyPosition[1], pacmanShut, 9, 9, SSD1306_WHITE);
+  else {
+    if (enemyDirection == 1) display.drawBitmap(enemyPosition[0], enemyPosition[1], pacmanOpenRight, 9, 9, SSD1306_WHITE);
+    else display.drawBitmap(enemyPosition[0], enemyPosition[1], pacmanOpenLeft, 9, 9, SSD1306_WHITE);
+  }
 }
 
 void drawPlatforms()
@@ -367,7 +367,6 @@ void drawWorld()
   display.drawLine(0, npcPlatformHeight, width, npcPlatformHeight, SSD1306_WHITE);
   drawPlatforms();
 }
-
 
 /*
 GAME OVER
@@ -403,7 +402,7 @@ void loop() {
     return;
   }
   if (isGameOver) {
-    if (input() == float(controls.up)) {
+    if (digitalRead(sButton) == HIGH){
       resetGame();
     }
     return;
